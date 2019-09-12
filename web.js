4905ee00edb2677381924711d012a47415bd64a6
Bert <z0bristbr@gmail.com>
2019-09-12 15:03:33 -0300

    setWebHook to HerokuApp

--- web.js
+++ web.js
@@ -1,17 +1,20 @@
 const Telegraf = require('telegraf')
 const BOT_TOKEN = '952727985:AAEX8gjCL_myd9RbEOuKTekfrgzaawhZT2M'
-const config = {
-	telegram: { webhookReply: true} // default true, but need to set false
-}
-const bot = new Telegraf(BOT_TOKEN, config)
+const bot = new Telegraf(BOT_TOKEN)
 
-bot.launch({
-  webhook: {
-    domain: 'https://acnlbrbot.herokuapp.com',
-    //'https://23f43f0f.ngrok.io',
-    port: 3000,
-  }
-})
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT)
+
+// bot.launch({
+//   webhook: {
+//     domain: 'https://acnlbrbot.herokuapp.com',
+//     //'https://23f43f0f.ngrok.io',
+//     port: 3000,
+//   }
+// })
 
 module.exports = {
     bot
