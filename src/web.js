3fcebee0abcc79d902de001bfc38832c38a4a1ce
Bert <z0bristbr@gmail.com>
2020-04-15 01:38:50 -0300

    BOT_TOKEN

--- src/web.js
+++ src/web.js
@@ -4,15 +4,15 @@ const ngrok = require('ngrok')
 
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN_TEST)
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
 async function start () {
   const externalHost = process.env.WEBHOOK_URL || await ngrok.connect(process.env.PORT)
 
-  await bot.telegram.setWebhook(`${externalHost}/${process.env.BOT_TOKEN_TEST}`)
+  await bot.telegram.setWebhook(`${externalHost}/${process.env.BOT_TOKEN}`)
   console.log(`Webhook set to ${externalHost}`)
 
-  bot.startWebhook(`/${process.env.BOT_TOKEN_TEST}`, null, process.env.PORT)
+  bot.startWebhook(`/${process.env.BOT_TOKEN}`, null, process.env.PORT)
   console.log(`Webhook listening on http://${externalHost}:${process.env.PORT}`)
 
   return bot
